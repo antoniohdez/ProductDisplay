08ec37f2b75d89c859085104cea3da269383abaa
Antonio <antonio.hdez93@gmail.com>
2014-01-16 21:37:16 -0600

    Elimina los archivos al eliminar el target, comentarios de código en archivo session.php

--- session.php
+++ session.php
@@ -4,16 +4,17 @@
 		$db = new handlerDB("productDisplay");
 		$email = $_POST["email"];
 		$password = $_POST["password"];
-		if(filter_var($email,FILTER_VALIDATE_EMAIL)){
+		if(filter_var($email,FILTER_VALIDATE_EMAIL)){//Valida que el formato de email sea valido
 			$statement = "SELECT * FROM user WHERE email = :email";
 			$query = $db->prepare($statement);
 			$query->bindParam(':email', $email, PDO::PARAM_STR);
 			$query->execute();
-			if($query->rowCount() === 1){
+			if($query->rowCount() === 1){//Debería regresar solamente un usuario
 				$result = $query->fetchAll(PDO::FETCH_ASSOC)[0];
 				$hashedPassword = $result["password"];
-				if(crypt($password, $hashedPassword) == $hashedPassword){
+				if(crypt($password, $hashedPassword) == $hashedPassword){//Verifica el password
 					session_start();
+					//Carga los datos del usuario para no traerlos desde la base de datos cada que se carga el index
 					$_SESSION["userInfo"]["id"] 	 = $result["id"];
 					$_SESSION["userInfo"]["name"] 	 = $result["name"];
 					$_SESSION["userInfo"]["country"] = $result["country"];
@@ -21,35 +22,36 @@
 					$_SESSION["userInfo"]["email"] 	 = $result["email"];
 					$_SESSION["userInfo"]["pathLogo"]= $result["path_logo"];
 
+					//Valida que la session activa en el servidor sea válida para productDisplay
 					$_SESSION["session"]["productDisplay"] = true;
 					$_SESSION["session"]["lastActivity"]   = time();
 					
 					if(isset($_POST["sessionTime"])){
-						$_SESSION["session"]["expirationTime"] = 60 * 60 * 24 * 2;//Session time, 2 day without activity.
+						$_SESSION["session"]["expirationTime"] = 60 * 60 * 24 * 2;//Tiempo de sesion, dos dias sin actividad
 					}else{
-						$_SESSION["session"]["expirationTime"] = 60 * 30;//Session time, 30 min without activity.
+						$_SESSION["session"]["expirationTime"] = 60 * 30;//Tiempo de sesion, 30 min sin actividad
 					}
-					header("Location: index.php");
+					header("Location: index.php");//Si se inicio sesion correctamente se envia al usuario al index
 				}else{
 					header("Location: login.php?error=wrongPassword");
 				}
 			}else{
-				header("Location: login.php?error=wrongUsername");
+				header("Location: login.php?error=wrongUsername");//El usuario no fue encontrado en la base de datos
 			}
 		}else{
-			header("Location: login.php?error=invalidEmail");
+			header("Location: login.php?error=invalidEmail");//El formato de email no es valido
 		}
 	}
 	else if(isset($_GET["logout"])){
 		session_start();
 		session_destroy();
-		if($_GET["logout"] === "expiredSession"){
+		if($_GET["logout"] === "expiredSession"){//En la funcion que valida la sesion se envia le argumento logout con el valor expiredSession
 			header("Location: login.php?warning=expiredSession");
 		}else{
-			header("Location: login.php");
+			header("Location: login.php");//Si la sesion la cierra el usuario se dirige al login normalmente
 		}
 	}
 	else{//Invalid action
-		header("Location: session.php?logout");
+		header("Location: session.php?logout");//El argumento enviado a session.php no es valido
 	}
 ?>
--- targetDelete.php
+++ targetDelete.php
@@ -3,6 +3,25 @@
 	if(isset($_POST["id"])){
 		$db = new handlerDB("productDisplay");
 		session_start();
+		//Se selecciona las rutas para borrar los archivos
+		$statement = "SELECT path_image, path_audio, path_video FROM target WHERE id = :id AND user_id = :user_id";
+		$query = $db->prepare($statement);
+		$query->bindParam(':id', $_POST["id"], PDO::PARAM_STR);
+		$query->bindParam(':user_id', $_SESSION["userInfo"]["id"], PDO::PARAM_STR);
+		$query->execute();
+		$result = $query->fetchAll(PDO::FETCH_ASSOC)[0];
+		var_dump($result);
+		//Elimina los archivos
+		unlink($result["path_image"]);
+		unlink($result["path_audio"]);
+		unlink($result["path_video"]);
+		
+		//=====//=====
+		//FALTA ELIMINAR LA IMAGEN GUARDADA EN CLOUD DATABASE DE VOFORIA
+		//=====//=====
+
+		//Se elimina el registro de la base de datos una vez que los archivos se borraron.
+		$db = new handlerDB("productDisplay");
 		$statement = "DELETE FROM target WHERE id = :id AND user_id = :user_id";
 		$query = $db->prepare($statement);
 		$query->bindParam(':id', $_POST["id"], PDO::PARAM_STR);
